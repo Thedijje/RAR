0f799921b8887dccb1cfedea2d2c74b9c1274d5e
Dheeraj jha <dheeraj@thedijje.com>
2018-12-15 20:18:44 +0530

    Menifest.json added, service worker added, yet to be configured, logo added

---
+++ sw.js
@@ -0,0 +1,35 @@
+//This is the "Offline page" service worker
+
+//Install stage sets up the offline page in the cache and opens a new cache
+self.addEventListener('install', function(event) {
+    var offlinePage = new Request('/offline');
+    event.waitUntil(
+      fetch(offlinePage).then(function(response) {
+        return caches.open('ror-offline').then(function(cache) {
+          console.log('ror-isntalled and offline caching done'+ response.url);
+          return cache.put(offlinePage, response);
+        });
+    }));
+  });
+  
+  //If any fetch fails, it will show the offline page.
+  //Maybe this should be limited to HTML documents?
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      fetch(event.request).catch(function(error) {
+        console.error( 'Failed to install sw ' + error );
+        return caches.open('pwabuilder-offline').then(function(cache) {
+          return cache.match('offline.html');
+        });
+      }
+    ));
+  });
+  
+  //This is a event that can be fired from your page to tell the SW to update the offline page
+  self.addEventListener('refreshOffline', function(response) {
+    return caches.open('ror-offline').then(function(cache) {
+      console.log('offline page updated '+ response.url);
+      return cache.put(offlinePage, response);
+    });
+  });
+  
